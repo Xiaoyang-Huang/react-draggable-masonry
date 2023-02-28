81ef4ddab2bdeb3d4a60d1755d85b90877dfd173
Xiaoyang Huang <hxy881004@gmail.com>
2023-02-28 17:31:34 +0800

    update dependencies

--- src/Masonry.ts
+++ src/Masonry.ts
@@ -1,6 +1,14 @@
-export * from "./component/ContainerWrapper";
-export * from "./component/ItemWrapper";
-export * from "./component/DragButton";
+import ContainerWrapper, { MasonryRef } from "./component/ContainerWrapper";
+import DragButton from "./component/DragButton";
+import ItemWrapper, { TileId } from "./component/ItemWrapper";
+import TileContext, { TileAgent } from "./context/TileContext";
+import TileManagerContext from "./context/TileManagerContext";
 
-export * from "./context/TileContext";
-export * from "./context/TileManagerContext";
+export type { MasonryRef, TileId, TileAgent };
+export { ContainerWrapper, DragButton, ItemWrapper, TileContext, TileManagerContext };
+
+// export * as ContainerWrapper from "./component/ContainerWrapper";
+// export * as DragButton from "./component/DragButton";
+// export * as ItemWrapper from "./component/ItemWrapper";
+// export * from "./context/TileContext";
+// export * from "./context/TileManagerContext";
